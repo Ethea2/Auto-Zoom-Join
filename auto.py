96b40f7644a66fb44d9e6c6ad33877eeaee70c88
Ethea2 <nathandres2001@gmail.com>
2021-11-16 17:12:52 +0400

    xd

--- auto.py
+++ auto.py
@@ -5,8 +5,6 @@ import webbrowser
 def openZoom(zoom_link):
 	webbrowser.open(zoom_link)
 
-#openZoom("""https://problemsolvingwithpython.com/07-Functions-and-Modules/07.05-Calling-Functions-from-Other-Files/""")
-
 # pyautogui.click(x=1916,y=1050, button="left")
 
 # pyautogui.press("win")
