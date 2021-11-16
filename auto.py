142fe24af29e84ff23f33a3e7f59dbf7e6fe2570
Ethea2 <nathandres2001@gmail.com>
2021-11-16 18:05:28 +0400

    working on the auto gui so it can cover most possibilities

--- auto.py
+++ auto.py
@@ -1,9 +1,17 @@
 import pyautogui
 import webbrowser
+import os
 
+location = os.path.realpath(
+			os.path.join(os.getcwd(), os.path.dirname(__file__)))
+img_folder = os.path.join(location, "images")
+
+waitPNG = f"{img_folder}\\wait.PNG"
 
 def openZoom(zoom_link):
 	webbrowser.open(zoom_link)
+	print(os.listdir(img_folder))
+
 
 # pyautogui.click(x=1916,y=1050, button="left")
 
